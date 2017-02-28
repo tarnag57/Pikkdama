17180589651540703183597369cbfa490b1cb71d
tarnag57 <toth.viktor7400@gmail.com>
2017-02-28 09:52:58 +0100

    Updated CradsDisplay

---
+++ CardsDisplay1/app/src/main/java/com/tarnag/cardsdisplay1/Card.java
@@ -0,0 +1,68 @@
+package com.tarnag.cardsdisplay1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by viktor on 2017. 02. 15..
+ */
+
+public class Card {
+    String type;  //X_YY
+    int colour;
+    int value;
+    String name;
+    String bmName = "x";
+
+
+    Card (String s){  //s=X_YY
+        type=s;
+        String value;
+        String colour;
+        value=s.substring(2);
+        this.value=Integer.parseInt(value);
+        colour=s.substring(0,1);
+        this.colour=Integer.parseInt(colour);
+        addName();
+    }
+
+    Card (int Colour,int Value){
+        colour=Colour;
+        value=Value;
+        type="";
+        type+=Integer.toString(Colour);
+        type+="_";
+        if (Value<10) type+="0";
+        type+=Integer.toString(Value);
+        addName();
+    }
+
+    //Adds name string to display for user
+    private void addName(){
+
+        String suit = "";
+        switch (colour) {
+            case 1: suit = "hearts"; break;
+            case 2: suit = "diamonds"; break;
+            case 3: suit = "spades"; break;
+            case 4: suit = "clubs"; break;
+        }
+
+        String symbol = "";
+        if (value < 11) {
+            symbol = Integer.toString(value);
+            bmName += symbol;
+        }
+        switch (value) {
+            case 11: symbol = "J"; bmName += "jack"; break;
+            case 12: symbol = "Q"; bmName += "queen"; break;
+            case 13: symbol = "K"; bmName += "king"; break;
+            case 14: symbol = "A"; bmName += "ace"; break;
+        }
+
+        name = suit + " " + symbol;
+
+        bmName += "_of_" + suit;
+    }
+}
--- CardsDisplay1/app/src/main/java/com/tarnag/cardsdisplay1/GamePanel.java
+++ CardsDisplay1/app/src/main/java/com/tarnag/cardsdisplay1/GamePanel.java
@@ -1,6 +1,7 @@
 package com.tarnag.cardsdisplay1;
 
 import android.content.Context;
+import android.gesture.Gesture;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -17,6 +18,20 @@ public class GamePanel extends SurfaceView implements Runnable{
 
     public static final int WIDTH = 1600;
     public static final int HEIGHT = 1200;
+    public static final int CARD_WIDTH = 500;
+    public static final int CARD_HEIGHT = 726;
+    public static final float HALF_CARD_SCALE = 0.4f;
+    public int desired_card_width;
+    public int desired_card_height;
+    public int desired_half_card_width;
+    public int screnWidth;
+    public int screenHeight;
+    public int cardsTop;
+    public int cardsLeft;
+    public int cardsRight;
+    public int cardsBottom;
+    private String TAG = MainActivity.class.getSimpleName();
+    float initialX, initialY;
 
     Thread thread = null;
     boolean canDraw = false;
@@ -24,28 +39,172 @@ public class GamePanel extends SurfaceView implements Runnable{
     Bitmap background;
     Canvas canvas;
     SurfaceHolder surfaceHolder;
+    Context context;
+
+    public boolean canPress = true;
+
+    //CARDS
+    Card[] cards = new Card[13];
 
     public GamePanel(Context context) {
         super(context);
-        surfaceHolder = getHolder();
+        this.context = context;
         background = BitmapFactory.decodeResource(getResources(), R.drawable.table_background);
+
+        //create cards
+        for (int i = 0; i < 13; i++) {
+            cards[i] = new Card(1, i+2);
+        }
     }
 
     @Override
     public void run() {
         while (canDraw) {
 
+            //determine screen width
+            screnWidth = getWidth();
+            screenHeight = getHeight();
+            surfaceHolder = getHolder();
+            desired_card_height = (int) ((double) screenHeight / 3.8);
+            desired_card_width = screnWidth / 9;
+            desired_half_card_width = (int) (desired_card_width * HALF_CARD_SCALE);
+
             //check whether the surface is valid
             if(!surfaceHolder.getSurface().isValid()) {
                 continue;
             }
 
+            canDraw = false;
             canvas = surfaceHolder.lockCanvas();
             Bitmap scaled = Bitmap.createScaledBitmap(background, getWidth(), getHeight(), false);
             canvas.drawBitmap(scaled, 0, 0, null);
+
+            drawCards(canvas);
+
             surfaceHolder.unlockCanvasAndPost(canvas);
             Log.d("run", "finished");
-            canDraw = false;
+        }
+    }
+
+    protected void drawCards(Canvas canvas) {
+        int size = cards.length;
+        cardsTop = (int) (screenHeight - 1.2 * desired_card_height);
+        cardsBottom = cardsTop + desired_card_height;
+        //computing the left and the right side of cards
+        int cardRowWidth = desired_card_width + (size - 1) * desired_half_card_width;
+        cardsLeft = screnWidth / 2 - cardRowWidth / 2;
+        cardsRight = cardsLeft + cardRowWidth;
+        int x = cardsLeft;
+        int y = cardsTop;
+        for (int i = 0; i < size; i++) {
+            //get bitmap
+            Log.d("Card number " + i, cards[i].bmName);
+            int id = getResources().getIdentifier(cards[i].bmName, "drawable", context.getPackageName());
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), id);
+            Log.d("des_card_width", String.valueOf(desired_card_width));
+            Log.d("des_card_height", String.valueOf(desired_card_height));
+            Bitmap scaled = Bitmap.createScaledBitmap(bm, desired_card_width, desired_card_height, false);
+            canvas.drawBitmap(scaled, x, y, null);
+            x += desired_half_card_width;
+        }
+    }
+
+    protected void drawLeftCards(int num, Canvas canvas) {
+
+    }
+
+    protected void drawTopCards(int num, Canvas canvas) {
+
+    }
+
+    protected void drawRightCards(int num, Canvas canvas) {
+
+    }
+
+    //handles user input
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                initialX = event.getX();
+                initialY = event.getY();
+
+                Log.d(TAG, "Action was DOWN");
+                Log.d("X coordinate", String.valueOf(initialX));
+                Log.d("Y coordinate", String.valueOf(initialY));
+
+                screePressed(initialX, initialY);
+
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "Action was MOVE");
+                break;
+
+            case MotionEvent.ACTION_UP:
+                float finalX = event.getX();
+                float finalY = event.getY();
+
+                Log.d(TAG, "Action was UP");
+
+                if (initialX < finalX) {
+                    Log.d(TAG, "Left to Right swipe performed");
+                }
+
+                if (initialX > finalX) {
+                    Log.d(TAG, "Right to Left swipe performed");
+                }
+
+                if (initialY < finalY) {
+                    Log.d(TAG, "Up to Down swipe performed");
+                }
+
+                if (initialY > finalY) {
+                    Log.d(TAG, "Down to Up swipe performed");
+                }
+
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG,"Action was CANCEL");
+                break;
+
+            case MotionEvent.ACTION_OUTSIDE:
+                Log.d(TAG, "Movement occurred outside bounds of current screen element");
+                break;
+        }
+
+        return super.onTouchEvent(event);
+
+    }
+
+    protected void selectedCard(int index) {
+        Log.d("Card was selected", cards[index].name);
+    }
+
+    private void screePressed(float x, float y) {
+        if (canPress) {
+            if ((cardsTop < y) && (y < cardsBottom)) {
+                if ((cardsLeft < x) && (x < cardsRight)) {
+                    int index = -1;
+                    int size = cards.length;
+                    int curLeft = cardsLeft;
+                    int curRight = curLeft + desired_half_card_width;
+                    for (int i = 0; i < size - 1; i++) {
+                        if ((curLeft < x) && (x < curRight)) {
+                            index = i;
+                            break;
+                        }
+                        curLeft = curRight;
+                        curRight += desired_half_card_width;
+                    }
+                    if (index == -1) {
+                        index = size - 1;
+                    }
+                    selectedCard(index);
+                }
+            }
         }
     }
 
